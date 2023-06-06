f276b0b6ead4a15c63ed101f005cff938eda791f
Yashanki123 <85667916+Yashanki123@users.noreply.github.com>
2023-06-06 17:07:17 +0530

    Update settings.py

--- digi/settings.py
+++ digi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-kr0&ge3sih&09i(!@o-qrtay1tv#-$53ll@^c)-bj@!es^^4$%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["137.184.48.136"]
 
